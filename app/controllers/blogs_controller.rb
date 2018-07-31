aab283746684a456227c55fe473bf67e7f23d578
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-07-31 15:21:04 +0900

    Edit create action article save

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,5 +8,11 @@ class BlogsController < ApplicationController
   end
 
   def create
+    Article.create(article_params)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:name, :text)
   end
 end
