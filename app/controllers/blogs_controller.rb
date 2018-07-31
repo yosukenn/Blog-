f2b5a18760e1bfa46cedd4ae87fc93d6f8768b96
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-07-31 19:29:31 +0900

    Add edit-function

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -20,6 +20,13 @@ class BlogsController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article = Article.find(params[:id])
+    article.update(article_params)
+    redirect_to action: :index
   end
 
   private
