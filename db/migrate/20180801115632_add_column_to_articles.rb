cc717f30a609fb4c987f99f6addad9ac3ed3f905
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-01 21:01:59 +0900

    Add migration remove user_id_id name

--- db/migrate/20180801115632_add_column_to_articles.rb
+++ db/migrate/20180801115632_add_column_to_articles.rb
@@ -1,5 +1,5 @@
 class AddColumnToArticles < ActiveRecord::Migration[5.0]
   def change
-    add_reference :articles, :user_id, foreign_key: true
+    add_reference :articles, :user, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180801105842) do
+ActiveRecord::Schema.define(version: 20180801115632) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.text     "text",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.integer  "user_id_id"
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
+    t.index ["user_id_id"], name: "index_articles_on_user_id_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -37,4 +41,5 @@ ActiveRecord::Schema.define(version: 20180801105842) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "articles", "users"
 end
