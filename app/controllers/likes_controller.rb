3868262cb06c92d70f97c05b8ada1b85d25e9d81
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-03 17:49:29 +0900

    Edit likes controller action create destroy

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,16 @@
 class LikesController < ApplicationController
+  def create
+    Like.create(like_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    like = Like.find(:id)
+    like.destroy
+  end
+
+  private
+  def like_params
+    params.permit(:article_id).merge(user_id: current_user.id)
+  end
 end
