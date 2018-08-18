e72b2d32bf83dde3ae497cbbaf8b928afb4fa5b6
田中陽祐 <lo.sa.wo.10969.already@gmail.com>
2018-08-18 14:55:29 +0900

    create comment table

--- db/migrate/20180818055227_create_comments.rb
+++ db/migrate/20180818055227_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.string :text
-      t.reference :user
-      t.reference :article
+      t.string :text, null: false
+      t.references :user, foreign_key: true
+      t.references :article, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180806141618) do
+ActiveRecord::Schema.define(version: 20180818055227) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "text",       limit: 65535
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20180806141618) do
     t.integer  "user_id"
     t.string   "title"
     t.index ["user_id"], name: "index_articles_on_user_id", using: :btree
+  end
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "text",       null: false
+    t.integer  "user_id"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
   create_table "likes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -49,6 +59,8 @@ ActiveRecord::Schema.define(version: 20180806141618) do
   end
 
   add_foreign_key "articles", "users"
+  add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "articles"
   add_foreign_key "likes", "users"
 end
